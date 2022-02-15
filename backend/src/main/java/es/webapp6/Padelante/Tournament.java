a2b46117c1085d18418d9aa1708c4e7bc184ea7e
DaniURJC <daniel.haro.universidad@gmail.com>
2022-02-15 17:39:55 +0100

    Generated getter and setter

--- backend/src/main/java/es/webapp6/Padelante/Tournament.java
+++ backend/src/main/java/es/webapp6/Padelante/Tournament.java
@@ -15,7 +15,7 @@ public class Tournament {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    private String name;
+    private String name; 
     private int numParticipants;
     private String about;
     private String ruleset;
@@ -39,7 +39,86 @@ public class Tournament {
                 this.startDate=startDate;
                 this.format=format;
                 this.tournamentImage=tournamentImage;
+    }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumParticipants() {
+        return numParticipants;
+    }
+
+    public void setNumParticipants(int numParticipants) {
+        this.numParticipants = numParticipants;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getRuleset() {
+        return ruleset;
+    }
+
+    public void setRuleset(String ruleset) {
+        this.ruleset = ruleset;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getInscriptionDate() {
+        return inscriptionDate;
+    }
+
+    public void setInscriptionDate(Date inscriptionDate) {
+        this.inscriptionDate = inscriptionDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getTournamentImage() {
+        return tournamentImage;
+    }
+
+    public void setTournamentImage(String tournamentImage) {
+        this.tournamentImage = tournamentImage;
     }
 
     
