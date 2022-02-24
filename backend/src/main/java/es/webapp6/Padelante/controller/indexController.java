f8449db6be272ae7d92af594cb60cb9929dc8136
alvaroga2175 <alvaroga2175@gmail.com>
2022-02-24 21:33:06 +0100

    code for future implemetation of images and who is connected

--- backend/src/main/java/es/webapp6/Padelante/controller/indexController.java
+++ backend/src/main/java/es/webapp6/Padelante/controller/indexController.java
@@ -1,26 +1,22 @@
 package es.webapp6.Padelante.controller;
 
 import java.security.Principal;
-import java.util.List;
+
 import java.util.Optional;
+
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.catalina.User;
-import org.hibernate.annotations.SourceType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.webapp6.Padelante.model.Tournament;
-import es.webapp6.Padelante.repositories.UserRepository;
 import es.webapp6.Padelante.service.TournamentService;
 import es.webapp6.Padelante.service.UserService;
 
@@ -80,7 +76,6 @@ public class indexController {
         return "errorPage";
     }
 
-   
 
     @GetMapping("/match")
     public String match(Model model) {
@@ -95,7 +90,6 @@ public class indexController {
     @PostMapping("/register")
     public String newRegister(Model model, @RequestParam String name, @RequestParam String encodedPassword){
         userService.registerNewUser(name, encodedPassword);
-        
         return "redirect:/";
     }
 
@@ -127,5 +121,99 @@ public class indexController {
 		}
 
 	}
+
+
+
+    // @GetMapping("/tournament/{id}/image")
+	// public ResponseEntity<Object> downloadImage(@PathVariable long id) throws SQLException {
+
+	// 	Optional<Tournament> tournament = tournamentService.findById(id);
+	// 	if (tournament.isPresent() && tournament.get().getImageFile() != null) {
+
+	// 		Resource file = new InputStreamResource(tournament.get().getImageFile().getBinaryStream());
+            
+	// 		return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
+	// 				.contentLength(tournament.get().getImageFile().length()).body(file);
+
+	// 	} else {
+	// 		return ResponseEntity.notFound().build();
+	// 	}
+	// }
+
+    // @GetMapping("/removeTournament/{id}")
+	// public String removeTournament(Model model, @PathVariable long id) {
+
+	// 	Optional<Tournament> tourn = tournamentService.findById(id);
+	// 	if (tourn.isPresent()) {
+	// 		tournamentService.delete(id);
+	// 		model.addAttribute("book", tourn.get());
+	// 	}
+	// 	return "removedbook";
+	// }
+
+    // @PostMapping("/newTournament")
+	// public String newTournaemntProcess(Model model, Book book, MultipartFile imageField, @RequestParam List<Long> selectedShops) throws IOException {
+
+	// 	if (!imageField.isEmpty()) {
+	// 		book.setImageFile(BlobProxy.generateProxy(imageField.getInputStream(), imageField.getSize()));
+	// 		book.setImage(true);
+	// 	}
+
+	// 	book.setShops(shopService.findById(selectedShops));
+
+	// 	bookService.save(book);
+
+	// 	model.addAttribute("bookId", book.getId());
+
+	// 	return "redirect:/books/"+book.getId();
+	// }
+
+    // @GetMapping("/editTournament/{id}")
+	// public String editTournament(Model model, @PathVariable long id) {
+
+	// 	Optional<Tournament> tourn = tournamentService.findById(id);
+	// 	if (tourn.isPresent()) {
+	// 		model.addAttribute("book", tourn.get());
+	// 		return "editBookPage";
+	// 	} else {
+	// 		return "books";
+	// 	}
+	// }
+
+	// @PostMapping("/editTournament")
+	// public String editTournamentProcess(Model model, Tournament tourn, boolean removeImage, MultipartFile imageField)
+	// 		throws IOException, SQLException {
+
+	// 	updateImage(tourn, removeImage, imageField);
+
+	// 	tournamentService.save(tourn);
+
+	// 	model.addAttribute("bookId", tourn.getId());
+
+	// 	return "redirect:/books/"+tourn.getId();
+	// }
+
+    // private void updateImage(Tournament tourn, boolean removeImage, MultipartFile imageField) throws IOException, SQLException {
+		
+	// 	if (!imageField.isEmpty()) {
+	// 		tourn.setImageFile(BlobProxy.generateProxy(imageField.getInputStream(), imageField.getSize()));
+	// 		tourn.setImage(true);
+	// 	} else {
+	// 		if (removeImage) {
+	// 			tourn.setImageFile(null);
+	// 			tourn.setImage(false);
+	// 		} else {
+	// 			// Maintain the same image loading it before updating the book
+	// 			Tournament dbTournament = tournamentService.findById(tourn.getId()).orElseThrow();
+	// 			if (dbTournament.getImage()) {
+	// 				tourn.setImageFile(BlobProxy.generateProxy(dbTournament.getImageFile().getBinaryStream(),
+    //                 dbTournament.getImageFile().length()));
+    //                         tourn.setImage(true);
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+
    
 }
--- backend/src/main/java/es/webapp6/Padelante/model/Tournament.java
+++ backend/src/main/java/es/webapp6/Padelante/model/Tournament.java
@@ -23,6 +23,11 @@ public class Tournament {
     private String format;
     private String tournamentImage;
 
+    // @Lob //for the image
+	// private Blob imageFile;
+
+	// private boolean image;
+
     public Tournament() {
         super();
     }
@@ -128,6 +133,21 @@ public class Tournament {
         this.tournamentImage = tournamentImage;
     }
 
-    
+
+    // public Blob getImageFile() {
+	// 	return imageFile;
+	// }
+
+	// public void setImageFile(Blob image) {
+	// 	this.imageFile = image;
+	// }
+
+	// public boolean getImage(){
+	// 	return this.image;
+	// }
+
+	// public void setImage(boolean image){
+	// 	this.image = image;
+	// }
 
 }
