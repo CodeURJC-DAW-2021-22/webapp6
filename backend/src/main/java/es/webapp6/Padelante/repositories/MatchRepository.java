5c6c6e91d8eb8baf86b0821701055b178c1b9dce
Ruben-urjc <r.catalan.medina@gmail.com>
2022-03-03 21:09:58 +0100

    getUserMatches query

--- backend/src/main/java/es/webapp6/Padelante/repositories/MatchRepository.java
+++ backend/src/main/java/es/webapp6/Padelante/repositories/MatchRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import es.webapp6.Padelante.model.Match;
 import es.webapp6.Padelante.model.Team;
 import es.webapp6.Padelante.model.Tournament;
+import es.webapp6.Padelante.model.User;
 
 public interface MatchRepository extends JpaRepository<Match,Long> {
     //it give me a list of the matches at tournament t
@@ -23,6 +24,9 @@ public interface MatchRepository extends JpaRepository<Match,Long> {
 
     @Query("SELECT m FROM Match m WHERE (m.teamOne = :t OR m.teamTwo = :t) AND m.round != 0 ORDER BY id ASC")
 	public List<Match> getTeamMatches(Team t);
+
+    @Query("SELECT m FROM Match m WHERE (m.teamOne.userA = :user OR m.teamOne.userB = :user OR m.teamTwo.userA = :user OR m.teamTwo.userB = :user) AND m.round != 0 ORDER BY id ASC")
+	public List<Match> getUserMatches(User user);
     
     @Query("SELECT m FROM Match m WHERE (m.teamOne = :t OR m.teamTwo = :t) AND m.round = 0 AND m.tournament = :tour ORDER BY id ASC")
 	public List<Match> getTeamAuxMatches(Tournament tour, Team t);
