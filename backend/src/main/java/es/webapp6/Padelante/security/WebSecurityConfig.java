d0bb5fb691c8b7fcde1f02430d06501f1c57fc6f
alvaroga2175 <alvaroga2175@gmail.com>
2022-03-03 18:00:12 +0100

    acces controll

--- backend/src/main/java/es/webapp6/Padelante/security/WebSecurityConfig.java
+++ backend/src/main/java/es/webapp6/Padelante/security/WebSecurityConfig.java
@@ -36,8 +36,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/login").permitAll();
         //http.authorizeRequests().antMatchers("/loginerror").permitAll();
         http.authorizeRequests().antMatchers("/logout").permitAll();
+        http.authorizeRequests().antMatchers("/register").permitAll();
+        http.authorizeRequests().antMatchers("/tourns/{id}").permitAll();
 
         // Private pages
+        http.authorizeRequests().antMatchers("/create_tournament").hasAnyRole("USER","ADMIN");
+        http.authorizeRequests().antMatchers("/user_profile").hasAnyRole("USER","ADMIN");
+        http.authorizeRequests().antMatchers("/user/{id}/image").hasAnyRole("USER","ADMIN");
+        http.authorizeRequests().antMatchers("/admin").hasAnyRole("ADMIN");
+        http.authorizeRequests().antMatchers("/tourns/{id}/image").hasAnyRole("USER","ADMIN");
         // http.authorizeRequests().antMatchers("/newbook").hasAnyRole("USER");
         // http.authorizeRequests().antMatchers("/editbook/*").hasAnyRole("USER");
         // http.authorizeRequests().antMatchers("/removebook/*").hasAnyRole("ADMIN");
