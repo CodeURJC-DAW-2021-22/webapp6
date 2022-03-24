04bc4297684b3e527dbc86c6d20c529f7186356c
Ruben-urjc <r.catalan.medina@gmail.com>
2022-03-24 23:20:20 +0100

    Tourn get by id fix + Tourn image API REST

--- backend/src/main/java/es/webapp6/padelante/controller/TournamentRestController.java
+++ backend/src/main/java/es/webapp6/padelante/controller/TournamentRestController.java
@@ -1,8 +1,14 @@
 package es.webapp6.padelante.controller;
 
+import java.io.IOException;
+import java.net.URI;
+import java.sql.SQLException;
 import java.util.Optional;
 
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +16,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import es.webapp6.padelante.model.Tournament;
 import es.webapp6.padelante.service.TournamentService;
@@ -19,21 +28,22 @@ import es.webapp6.padelante.service.TournamentService;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 @RestController
 @RequestMapping("/api/tournaments")
 public class TournamentRestController {
-    @Autowired
+	@Autowired
 	private TournamentService tournamentService;
 
-	//get all users
-    //Doesnt work on Postman because of PAGEABLE
-	@GetMapping("?page={page}")
-	public Page<Tournament> getTournaments(@PathVariable int page) {
+	// get all users
+	// Only shows 4 of 6 tourns on the 1st page. FIX IT
+	@GetMapping("")
+	public Page<Tournament> getTournaments(@RequestParam int page) {
 		return tournamentService.getTournaments(page);
 	}
 
-	//get tournament by id
+	// get tournament by id
 	@GetMapping("/{id}")
 	public ResponseEntity<Optional<Tournament>> getTournament(@PathVariable long id) {
 
@@ -45,14 +55,15 @@ public class TournamentRestController {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
+
 	@PostMapping("")
-	@ResponseStatus (HttpStatus.CREATED)
-	public Tournament createTournament(@RequestBody Tournament tournament) {
+	public ResponseEntity<Tournament> createTournament(@RequestBody Tournament tournament) {
 
 		tournamentService.save(tournament);
 
-		return tournament;
+		URI location = fromCurrentRequest().path("/{id}").buildAndExpand(tournament.getId()).toUri();
+
+		return ResponseEntity.created(location).body(tournament);
 	}
 
 	@DeleteMapping("/{id}")
@@ -65,5 +76,55 @@ public class TournamentRestController {
 		} catch (EmptyResultDataAccessException e) {
 			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 		}
+	}
+
+	@PostMapping("/{id}/image")
+	public ResponseEntity<Object> uploadImage(@PathVariable long id, @RequestParam MultipartFile imageFile)
+			throws IOException {
+
+		Optional<Tournament> tournament = tournamentService.findById(id);
+
+		if (tournament != null){
+			URI location = fromCurrentRequest().build().toUri();
+
+			tournament.get().setImage(true);
+			tournament.get().setImageFile(BlobProxy.generateProxy(imageFile.getInputStream(), imageFile.getSize()));
+			tournamentService.save(tournament.get());
+
+			return ResponseEntity.created(location).build();
+		}
+		else{
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/{id}/image")
+	public ResponseEntity<Object> downloadImage(@PathVariable long id) throws SQLException {
+
+		Tournament tournament = tournamentService.findById(id).orElseThrow();
+
+		if (tournament.getImageFile() != null) {
+
+			Resource file = new InputStreamResource(tournament.getImageFile().getBinaryStream());
+
+			return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
+					.contentLength(tournament.getImageFile().length()).body(file);
+
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/{id}/image")
+	public ResponseEntity<Object> deleteImage(@PathVariable long id) throws IOException {
+
+		Tournament tournament = tournamentService.findById(id).orElseThrow();
+
+		tournament.setImageFile(null);
+		tournament.setImage(false);
+
+		tournamentService.save(tournament);
+
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/es/webapp6/padelante/model/Tournament.java
+++ backend/src/main/java/es/webapp6/padelante/model/Tournament.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Tournament {
 
@@ -29,6 +31,7 @@ public class Tournament {
     private Date inscriptionDate;
     private Date startDate;
    
+    @JsonIgnore 
     @Lob
 	private Blob imageFile;
 
