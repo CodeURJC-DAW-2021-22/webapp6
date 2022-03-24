59a42d89172a5442f8e92a2d0c2fe33cf4d85bc8
Ruben-urjc <r.catalan.medina@gmail.com>
2022-03-24 20:38:56 +0100

    Tournament basic APIs (get by id & create no work)

--- backend/src/main/java/es/webapp6/padelante/controller/TournamentRestController.java
+++ backend/src/main/java/es/webapp6/padelante/controller/TournamentRestController.java
@@ -1,15 +1,8 @@
 package es.webapp6.padelante.controller;
 
-import java.security.Principal;
-import java.util.Collection;
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,32 +10,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
 import es.webapp6.padelante.model.Tournament;
-import es.webapp6.padelante.model.User;
-import es.webapp6.padelante.service.MatchService;
-import es.webapp6.padelante.service.TeamService;
 import es.webapp6.padelante.service.TournamentService;
-import es.webapp6.padelante.service.UserService;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+
 
 @RestController
 @RequestMapping("/api/tournaments")
 public class TournamentRestController {
     @Autowired
-	private TournamentService tournamentService;	
-
-    @Autowired
-	private UserService userService;	
-
-	@Autowired
-	private MatchService matchService;	
-
-	@Autowired
-	private TeamService teamService;
-
+	private TournamentService tournamentService;
 
 	//get all users
     //Doesnt work on Postman because of PAGEABLE
@@ -63,7 +45,25 @@ public class TournamentRestController {
 			return ResponseEntity.notFound().build();
 		}
 	}
-    
-
 	
+	@PostMapping("")
+	@ResponseStatus (HttpStatus.CREATED)
+	public Tournament createTournament(@RequestBody Tournament tournament) {
+
+		tournamentService.save(tournament);
+
+		return tournament;
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Tournament> deleteTournament(@PathVariable long id) {
+
+		try {
+			tournamentService.delete(id);
+			return new ResponseEntity<>(null, HttpStatus.OK);
+
+		} catch (EmptyResultDataAccessException e) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+	}
 }
