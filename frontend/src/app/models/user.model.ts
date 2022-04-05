82b961d5c38f046a7f52756315d612f6592883a2
alvaroga2175 <alvaroga2175@gmail.com>
2022-04-05 09:38:01 +0200

    user.model.ts created

---
+++ frontend/src/app/models/user.model.ts
@@ -0,0 +1,18 @@
+export interface User{
+    id?:number;
+    name: string;
+    email: string;
+    realName:string;
+    location:string;
+    country:string;
+    phone:string;
+    numWins:number;
+    numLoses:number;
+    numMatchesPlayed:number;
+    historicalKarma: number[];
+    status:boolean;
+    imageFile: Blob;
+    image:boolean;
+    encodedPassword:string;
+
+}
