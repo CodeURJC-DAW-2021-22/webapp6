3672aa95890b142b27df44f020e0ed1c4167b554
Ruben-urjc <r.catalan.medina@gmail.com>
2022-04-06 14:21:39 +0200

    Login redirect to main when completed

--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -2,6 +2,7 @@ import { throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
+import { Router } from '@angular/router';
 
 const BASE_URL = '/api/auth';
 
@@ -11,53 +12,54 @@ export class LoginService {
   logged: boolean | undefined;
   user: User | undefined;
 
-  constructor(private http: HttpClient) {
-      this.reqIsLogged();
+  constructor(private http: HttpClient, private router: Router) {
+    this.reqIsLogged();
   }
 
   reqIsLogged() {
 
-      this.http.get('/api/users/me', { withCredentials: true }).subscribe(
-          response => {
-              this.user = response as User;
-              this.logged = true;
-          },
-          error => {
-              if (error.status != 403) {
-                  console.error('Error when asking if logged: ' + JSON.stringify(error));
-              }
-          }
-      );
+    this.http.get('/api/users/me', { withCredentials: true }).subscribe(
+      response => {
+        this.user = response as User;
+        this.logged = true;
+      },
+      error => {
+        if (error.status != 403) {
+            console.error('Error when asking if logged: ' + JSON.stringify(error));
+        }
+      }
+    );
 
   }
 
   logIn(username: string, password: string) {
 
-      this.http.post(BASE_URL + "/login", { username: username, password: password }, { withCredentials: true })
-          .subscribe(
-              (response) => this.reqIsLogged(),
-              (error) => alert("Wrong credentials")
-          );
+    this.http.post(BASE_URL + "/login", { username: username, password: password }, { withCredentials: true }).subscribe(
+        (response) => {
+          this.reqIsLogged();
+          this.router.navigate(['']);
+        },
+        (error) => alert("Wrong credentials")
+    );
 
   }
 
   logOut() {
 
-      return this.http.post(BASE_URL + '/logout', { withCredentials: true })
-          .subscribe((resp: any) => {
-              console.log("LOGOUT: Successfully");
-              this.logged = false;
-              this.user = undefined;
-          });
+    return this.http.post(BASE_URL + '/logout', { withCredentials: true }).subscribe((resp: any) => {
+        console.log("LOGOUT: Successfully");
+        this.logged = false;
+        this.user = undefined;
+    });
 
   }
 
   isLogged() {
-      return this.logged;
+    return this.logged;
   }
 
   isAdmin() {
-      return this.user && this.user.roles.indexOf('ADMIN') !== -1;
+    return this.user && this.user.roles.indexOf('ADMIN') !== -1;
   }
 
   currentUser():User {
