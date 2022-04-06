7517ada805cf43b6b45e0169f60b088d8122f2ba
alvaroga2175 <alvaroga2175@gmail.com>
2022-04-06 13:00:19 +0200

    register class created

---
+++ frontend/src/app/components/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { UserService } from 'src/app/services/user.service';
+
+
+@Component({
+  templateUrl: './register.component.html'
+})
+
+export class RegisterComponent {
+
+  constructor() {}
+
+
+
+}
