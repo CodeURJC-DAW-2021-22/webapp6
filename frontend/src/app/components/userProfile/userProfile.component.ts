ada658d926fe536b69d8fed3677b2996233afa38
alvaroga2175 <alvaroga2175@gmail.com>
2022-04-09 18:47:09 +0200

    An user now can be update

--- frontend/src/app/components/userProfile/userProfile.component.ts
+++ frontend/src/app/components/userProfile/userProfile.component.ts
@@ -2,6 +2,7 @@ import { User } from '../../models/user.model';
 import { Component, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { LoginService } from 'src/app/services/login.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 
@@ -11,7 +12,9 @@ import { LoginService } from 'src/app/services/login.service';
 })
 
 export class UserProfileComponent{
-  constructor(public loginService: LoginService, public userService: UserService) { }
+
+  constructor(private router: Router,public loginService: LoginService, public userService: UserService,activatedRoute: ActivatedRoute) {
+   }
 
   calculatedKarma( array : number[]){
     let j = 0;
@@ -20,4 +23,12 @@ export class UserProfileComponent{
       }
     return Math.round(j/array.length);
   }
+
+  updateUserForm(event:any,fullName:string,location:string, country:string, phone:string,user:User){
+      user.realName=fullName;
+      user.location = location;
+      user.country=country;
+      user.phone = phone;
+      this.userService.updateUser(user);
+    }
 }
