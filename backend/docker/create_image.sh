6d15e51fcc972c5add21f7acc6df2a853df8beeb
Ruben-urjc <r.catalan.medina@gmail.com>
2022-04-26 20:25:19 +0200

    Script update + some other fix

--- backend/docker/create_image.sh
+++ backend/docker/create_image.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+cd ../../frontend
+
+npm install
+
+ng build --prod --base-href="/new/"
+
+cp -r dist/frontend/* ../backend/src/main/resources/public/new/
+
+cd ../backend/docker
+
 docker build -t $1/padelante_web .. -f ./Dockerfile
 
-# docker push $1/padelante_web
+docker push $1/padelante_web
+
